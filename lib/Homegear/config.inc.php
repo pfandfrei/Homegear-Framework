5a6677650deecb55df10eaf7d9e2dc77ff00954e
pfandfrei <pfandfrei@users.noreply.github.com>
2016-10-18 21:06:19 +0200

    Create config.inc.php

---
+++ lib/Homegear/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = 
+[
+    'tz' => 'Europe/Berlin',    // default timezone for timestamps
+    'host' => '192.168.2.124',  // homegear ip
+    'port' => 2001              // Homegear port for XMLRPC
+];
+?>
